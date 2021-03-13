bdab445acfd10ef705226e4f9642ac34f447ab18
Roger Schmid <rs@roguelabs.net>
2021-03-13 18:42:54 +0100

    create mac variant

---
+++ mac_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="vimrc vim tmux.conf gitconfigc zshrc"
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv -v ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -sv $dir/$file-mac ~/.$file
+done
